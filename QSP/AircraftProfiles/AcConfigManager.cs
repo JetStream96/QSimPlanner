75787462af7012619e518930619ab555dbb73389
David Chen <davidchen9568@gmail.com>
2016-04-14 22:16:09 +0800

    Implement Validate().

--- QSP/AircraftProfiles/AcConfigManager.cs
+++ QSP/AircraftProfiles/AcConfigManager.cs
@@ -71,11 +71,49 @@ namespace QSP.AircraftProfiles
             registrations = new Dictionary<string, AircraftConfig>();
         }
 
+        /// <summary>
+        /// Check whether the takeoff and landing performance files exist
+        /// for all aircraft configs. 
+        /// </summary>
         /// <exception cref="PerfFileNotFoundException"></exception>
         public void Validate(List<TOPerfCalculation.PerfTable> takeoffTables,
                              List<LandingPerfCalculation.PerfTable> ldgTables)
         {
+            var invalidAc = new List<AircraftConfig>();
 
+            foreach (var i in registrations)
+            {
+                var config = i.Value;
+                bool toNotFound = takeoffTables.FirstOrDefault(
+                    x => x.Entry.FilePath == config.TOPerfFile) == null;
+
+                bool ldgNotFound = ldgTables.FirstOrDefault(
+                    x => x.Entry.FilePath == config.LdgPerfFile) == null;
+
+                if (toNotFound || ldgNotFound)
+                {
+                    invalidAc.Add(config);
+                }
+            }
+
+            if (invalidAc.Count > 0)
+            {
+                throw new PerfFileNotFoundException(errorMsg(invalidAc));
+            }
+        }
+
+        private static string errorMsg(List<AircraftConfig> invalidItems)
+        {
+            var msg = new StringBuilder(
+                 "Cannot find takeoff/landing performance profiles " +
+                 "for the following aircraft(s):\n");
+
+            foreach (var i in invalidItems)
+            {
+                msg.AppendLine(i.Registration + " (" + i.AC + ")");
+            }
+
+            return msg.ToString();
         }
 
         /// <summary>
