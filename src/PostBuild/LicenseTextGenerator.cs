4c344082087943d1022aea0c97ee656dd9f3254c
David Chen <davidchen9568@gmail.com>
2016-05-18 19:36:44 +0800

    Correct typo.

--- src/PostBuild/LicenseTextGenerator.cs
+++ src/PostBuild/LicenseTextGenerator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
 
-namespace PostBuilt
+namespace PostBuild
 {
     public static class LicenseTextGenerator
     {
