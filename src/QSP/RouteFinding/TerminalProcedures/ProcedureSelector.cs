d3017a3853a1b46c24919d534011e617dfc07a4c
David Chen <davidchen9568@gmail.com>
2016-06-17 22:28:41 +0800

    Clarify the logic and greatly improve readability.

--- src/QSP/RouteFinding/TerminalProcedures/ProcedureSelector.cs
+++ src/QSP/RouteFinding/TerminalProcedures/ProcedureSelector.cs
@@ -13,12 +13,12 @@ namespace QSP.RouteFinding.TerminalProcedures
     // (2) An entry with a procedure that does not have a runway-specific 
     //     part in the list, is usable regardless of runway.
     //     Therefore it's selected.
-    // (3) An entry with a procedure that have a runway-specific part in 
-    //     the list, but none of the runway-specific parts matches the 
-    //     given runway, is not selected.
+    // (3) If the procedure of an entry has runway-specific part, but 
+    //     none of the runway-specific parts matches the given 
+    //     runway, it is not selected.
     // (4) An entry which is a transition is selected if and only if
-    //     at least one entry with the same procedure name (ProcedureName) 
-    //     is selected by the criterions above.
+    //     at least one entry with the same ProcedureName is selected 
+    //     by the criterions above.
     // (5) If a procedure has transitions then it cannot appear as 
     //     one without transition. 
     //     e.g. If the selected procedures are SID1, SID1.TRANS1, 
@@ -43,100 +43,53 @@ namespace QSP.RouteFinding.TerminalProcedures
         /// </summary>
         public List<string> GetProcedureList()
         {
-            List<string> noTrans;
-            List<TerminalProcedureName> trans;
+            var groups = procedureEntries.GroupBy(p => p.Name);
 
-            classifyProcedures(out noTrans, out trans);
+            // Rule (1) - (4) satisfied
+            var filterd =
+                groups.Where(g =>
+                isRunwaySpecific(g) == false || containDesiredRwy(g));
 
-            // Remove transitions that are not available for the runway.
-            removeTransitionWrongRunway(noTrans, trans);
+            // Rule (5)
+            var names = filterd.SelectMany(f => fullNames(f));
 
-            // Procedures which have transition(s), should not appear as 
-            // one without transition.
-            removeProcedureWithoutTransition(noTrans, trans);
-
-            // Merge the two lists
-            foreach (var k in trans)
-            {
-                noTrans.Add(k.ProcedureName + '.' + k.TransitionName);
-            }
-            return noTrans;
+            return names.ToList();
         }
 
-        private void classifyProcedures(
-            out List<string> noTrans, out List<TerminalProcedureName> trans)
+        private IEnumerable<string> fullNames(IGrouping<string, T> group)
         {
-            noTrans = new List<string>();
-            trans = new List<TerminalProcedureName>();
+            string name = null;
+            bool hasTransition = false;
 
-            foreach (var i in procedureEntries)
+            foreach (var i in group)
             {
                 if (i.Type == EntryType.Transition)
                 {
-                    trans.Add(
-                        new TerminalProcedureName(
-                            i.Name, i.RunwayOrTransition));
+                    hasTransition = true;
+                    yield return i.Name + "." + i.RunwayOrTransition;
                 }
-                else if (
-                    (i.Type == EntryType.RwySpecific &&
-                    i.RunwayOrTransition == rwy) ||
-                    (i.Type == EntryType.Common &&
-                    runwaySpecificPartExists(i.Name) == false))
+                else
                 {
-                    // RwySpecific or common parts which are available 
-                    // for the particular runway.
-                    noTrans.Add(i.Name);
+                    name = i.Name;
                 }
             }
 
-            // Remove duplicates, from runway specific part and common part
-            noTrans = noTrans.Distinct().ToList();
-        }
-
-        private bool runwaySpecificPartExists(string procedureName)
-        {
-            return procedureEntries.Exists(i =>
-            i.Type == EntryType.RwySpecific &&
-            i.Name == procedureName);
-        }
-
-        private static void removeTransitionWrongRunway(
-            List<string> noTrans, List<TerminalProcedureName> trans)
-        {
-            for (int i = trans.Count - 1; i >= 0; i--)
+            if (hasTransition == false)
             {
-                bool removeEntry = true;
-
-                foreach (var j in noTrans)
-                {
-                    if (j == trans[i].ProcedureName)
-                    {
-                        removeEntry = false;
-                        break;
-                    }
-                }
-
-                if (removeEntry)
-                {
-                    trans.RemoveAt(i);
-                }
+                yield return name;
             }
         }
 
-        private static void removeProcedureWithoutTransition(
-            List<string> noTrans, List<TerminalProcedureName> trans)
+        private bool containDesiredRwy(IGrouping<string, T> group)
         {
-            for (int i = noTrans.Count - 1; i >= 0; i--)
-            {
-                foreach (var j in trans)
-                {
-                    if (noTrans[i] == j.ProcedureName)
-                    {
-                        noTrans.RemoveAt(i);
-                        break;
-                    }
-                }
-            }
+            return group.Any(
+                x => x.Type == EntryType.RwySpecific &&
+                x.RunwayOrTransition == rwy);
+        }
+
+        private bool isRunwaySpecific(IGrouping<string, T> group)
+        {
+            return group.Any(i => i.Type == EntryType.RwySpecific);
         }
     }
 }
