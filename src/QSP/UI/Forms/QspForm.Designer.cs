ee1c851923545a4e4cad9c432786e639a3a76d06
David Chen <davidchen9568@gmail.com>
2016-07-05 00:23:33 +0800

    Add buttons to form.

--- src/QSP/UI/Forms/QspForm.Designer.cs
+++ src/QSP/UI/Forms/QspForm.Designer.cs
@@ -28,7 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.aboutBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.optionsBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.airportBtn = new System.Windows.Forms.Button();
+            this.ldgBtn = new System.Windows.Forms.Button();
+            this.acConfigBtn = new System.Windows.Forms.Button();
+            this.toBtn = new System.Windows.Forms.Button();
+            this.fuelBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.aboutBtn, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(1125, 9);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(40, 40);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // aboutBtn
+            // 
+            this.aboutBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.aboutBtn.BackColor = System.Drawing.Color.Turquoise;
+            this.aboutBtn.BackgroundImage = global::QSP.Properties.Resources.info;
+            this.aboutBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.aboutBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aboutBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.aboutBtn.Location = new System.Drawing.Point(5, 5);
+            this.aboutBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.aboutBtn.Name = "aboutBtn";
+            this.aboutBtn.Size = new System.Drawing.Size(30, 30);
+            this.aboutBtn.TabIndex = 2;
+            this.aboutBtn.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.optionsBtn, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1086, 9);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(40, 40);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // optionsBtn
+            // 
+            this.optionsBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.optionsBtn.BackColor = System.Drawing.Color.Purple;
+            this.optionsBtn.BackgroundImage = global::QSP.Properties.Resources.noun_1329;
+            this.optionsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.optionsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optionsBtn.Location = new System.Drawing.Point(5, 5);
+            this.optionsBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.optionsBtn.Name = "optionsBtn";
+            this.optionsBtn.Size = new System.Drawing.Size(30, 30);
+            this.optionsBtn.TabIndex = 0;
+            this.optionsBtn.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Controls.Add(this.fuelBtn, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.acConfigBtn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.airportBtn, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ldgBtn, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.toBtn, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 9);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(755, 41);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // airportBtn
+            // 
+            this.airportBtn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.airportBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.airportBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.airportBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.airportBtn.Location = new System.Drawing.Point(605, 1);
+            this.airportBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.airportBtn.Name = "airportBtn";
+            this.airportBtn.Size = new System.Drawing.Size(150, 38);
+            this.airportBtn.TabIndex = 3;
+            this.airportBtn.Text = "Airports";
+            this.airportBtn.UseVisualStyleBackColor = false;
+            // 
+            // ldgBtn
+            // 
+            this.ldgBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ldgBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ldgBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ldgBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ldgBtn.Location = new System.Drawing.Point(454, 1);
+            this.ldgBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.ldgBtn.Name = "ldgBtn";
+            this.ldgBtn.Size = new System.Drawing.Size(150, 38);
+            this.ldgBtn.TabIndex = 2;
+            this.ldgBtn.Text = "Landing";
+            this.ldgBtn.UseVisualStyleBackColor = false;
+            // 
+            // acConfigBtn
+            // 
+            this.acConfigBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.acConfigBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.acConfigBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acConfigBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.acConfigBtn.Location = new System.Drawing.Point(1, 1);
+            this.acConfigBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.acConfigBtn.Name = "acConfigBtn";
+            this.acConfigBtn.Size = new System.Drawing.Size(150, 38);
+            this.acConfigBtn.TabIndex = 0;
+            this.acConfigBtn.Text = "Aircraft Config";
+            this.acConfigBtn.UseVisualStyleBackColor = false;
+            // 
+            // toBtn
+            // 
+            this.toBtn.BackColor = System.Drawing.Color.ForestGreen;
+            this.toBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toBtn.Location = new System.Drawing.Point(303, 1);
+            this.toBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.toBtn.Name = "toBtn";
+            this.toBtn.Size = new System.Drawing.Size(150, 38);
+            this.toBtn.TabIndex = 1;
+            this.toBtn.Text = "Take off";
+            this.toBtn.UseVisualStyleBackColor = false;
+            // 
+            // fuelBtn
+            // 
+            this.fuelBtn.BackColor = System.Drawing.Color.DarkOrange;
+            this.fuelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fuelBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fuelBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.fuelBtn.Location = new System.Drawing.Point(152, 1);
+            this.fuelBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.fuelBtn.Name = "fuelBtn";
+            this.fuelBtn.Size = new System.Drawing.Size(150, 38);
+            this.fuelBtn.TabIndex = 7;
+            this.fuelBtn.Text = "Fuel Planning";
+            this.fuelBtn.UseVisualStyleBackColor = false;
             // 
             // QspForm
             // 
@@ -36,13 +198,30 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1182, 755);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "QspForm";
             this.ShowIcon = false;
             this.Text = "QspForm";
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button aboutBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button optionsBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button airportBtn;
+        private System.Windows.Forms.Button ldgBtn;
+        private System.Windows.Forms.Button acConfigBtn;
+        private System.Windows.Forms.Button toBtn;
+        private System.Windows.Forms.Button fuelBtn;
     }
 }
