3668f8f5aa7a5ff942b17c3afa5a28b083155c5d
David Chen <davidchen9568@gmail.com>
2016-07-19 18:39:39 +0800

    Change exception handling when checking airac dates.

--- src/QSP/Utilities/AiracTools.cs
+++ src/QSP/Utilities/AiracTools.cs
@@ -9,45 +9,41 @@ namespace QSP.Utilities
         /// Determines whether the AIRAC cycle is within valid period.
         /// </summary>
         /// <param name="airacPeriod">For example, 26JUN23JUL/14.</param>
+        /// <exception cref="ArgumentException"></exception>
         public static bool AiracValid(string airacPeriod)
         {
-            int monthStart = 0;
-            int monthEnd = 0;
-            int dayStart = 0;
-            int dayEnd = 0;
-            int yearStart = 0;
-            int yearEnd = 0;
-
             try
             {
-                monthStart = MonthEnglishToNum(airacPeriod.Substring(2, 3));
-                monthEnd = MonthEnglishToNum(airacPeriod.Substring(7, 3));
-                dayStart = Convert.ToInt32(airacPeriod.Substring(0, 2));
-                dayEnd = Convert.ToInt32(airacPeriod.Substring(5, 2));
-                yearStart = Convert.ToInt32(airacPeriod.Substring(11, 2));
+                var a = airacPeriod;
+                int monthStart = MonthEnglishToNum(a.Substring(2, 3));
+                int monthEnd = MonthEnglishToNum(a.Substring(7, 3));
+                int dayStart = Convert.ToInt32(a.Substring(0, 2));
+                int dayEnd = Convert.ToInt32(a.Substring(5, 2));
+                int yearStart = Convert.ToInt32(a.Substring(11, 2));
+                int yearEnd;
+
+                if (monthEnd < monthStart)
+                {
+                    yearEnd = yearStart + 1;
+                }
+                else
+                {
+                    yearEnd = yearStart;
+                }
+
+                var dateStart =
+                    new DateTime(2000 + yearStart, monthStart, dayStart);
+
+                var dateEnd = new DateTime(2000 + yearEnd, monthEnd, dayEnd);
+                var dateNow = DateTime.UtcNow.Date;
+
+                return (dateStart <= dateNow && dateNow <= dateEnd);
             }
             catch
             {
                 throw new ArgumentException(
-                    "Input string is not valid airac period.");
+                   "Input string is not valid airac period.");
             }
-
-            if (monthEnd < monthStart)
-            {
-                yearEnd = yearStart + 1;
-            }
-            else
-            {
-                yearEnd = yearStart;
-            }
-
-            var dateStart =
-                new DateTime(2000 + yearStart, monthStart, dayStart);
-
-            var dateEnd = new DateTime(2000 + yearEnd, monthEnd, dayEnd);
-            var dateNow = DateTime.UtcNow.Date;
-
-            return (dateStart <= dateNow && dateNow <= dateEnd);
         }
     }
 }
