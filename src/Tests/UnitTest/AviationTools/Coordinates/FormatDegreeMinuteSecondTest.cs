e5a6bcf78d2422c3a8b23a378a394b6c594ebdec
David Chen <davidchen9568@gmail.com>
2016-08-14 21:32:02 +0800

    Add test for the new lat/lon format.

---
+++ src/Tests/UnitTest/AviationTools/Coordinates/FormatDegreeMinuteSecondTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using static QSP.AviationTools.Coordinates.FormatDegreeMinuteSecond;
+
+namespace UnitTest.AviationTools.Coordinates
+{
+    public class FormatDegreeMinuteSecondTest
+    {
+        public void CustomFormatTest()
+        {
+            var result = ToDegreeMinuteSecondFormat(25.073133333, "F2");
+            Assert.IsTrue("25° 4' 23.28\"" == result);
+        }
+    }
+}
