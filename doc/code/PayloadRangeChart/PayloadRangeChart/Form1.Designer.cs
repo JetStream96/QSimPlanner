eb62bd02a1d2bb1d0b7a3d20d247b647ff4d2bc7
David Chen <davidchen9568@gmail.com>
2016-08-22 22:22:15 +0800

    Add docuemntation regarding payload-fuel diagrams.

---
+++ doc/code/PayloadRangeChart/PayloadRangeChart/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PayloadRangeChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(801, 492);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 516);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ doc/code/PayloadRangeChart/PayloadRangeChart/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PayloadRangeChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PlotPayloadRangeChart();
+        }
+
+        private static double fuelCapacityTon = 20.894;
+        private static double reserveFuelTon =
+            (38.0 * 45.0 +  // Final reserve
+            130.0)          // One go-around
+            / 1000.0;
+
+        private static double contingencyPercent = 5.0;
+
+        private static double cruiseSpeedKnots = 460.0;
+
+        private void PlotPayloadRangeChart()
+        {
+            double zfw = 40.0;
+
+            while (zfw <= 55.0)
+            {
+                chart1.Series["Series1"].Points.AddXY(GetRangePara1(zfw), zfw);
+                zfw += 0.1;
+            }
+
+            chart1.Series["Series1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["Series1"].Color = Color.DodgerBlue;
+
+            // Adjust these parameters according to data range.
+            chart1.ChartAreas[0].AxisX.Maximum = 5000;
+            chart1.ChartAreas[0].AxisX.Minimum = 3500;
+            chart1.ChartAreas[0].AxisY.Maximum = 60;
+            chart1.ChartAreas[0].AxisY.Minimum = 35;
+        }
+
+        private static double GetRangePara1(double zfwTon)
+        {
+            const double deltaTimeHour = 0.01;
+            double totalTime = 0.0;
+            double grossWeightTon = zfwTon + fuelCapacityTon;
+
+            while (grossWeightTon > zfwTon)
+            {
+                double ff = FuelFlowTonPerHour(grossWeightTon);
+                grossWeightTon -= ff * deltaTimeHour;
+                totalTime += deltaTimeHour;
+            }
+
+            return totalTime * cruiseSpeedKnots;
+        }
+
+        private static double GetRangePara2(double zfwTon)
+        {
+            const double deltaTimeHour = 0.01;
+            double totalTime = 0.0;
+            double grossWeightTon = zfwTon + fuelCapacityTon;
+
+            double actualUsableFuelTon = (fuelCapacityTon - reserveFuelTon) /
+                (1.0 + contingencyPercent / 100.0);
+
+            while (actualUsableFuelTon > 0.0)
+            {
+                double ff = FuelFlowTonPerHour(grossWeightTon);
+                grossWeightTon -= ff * deltaTimeHour;
+                actualUsableFuelTon -= ff * deltaTimeHour;
+                totalTime += deltaTimeHour;
+            }
+
+            return totalTime * cruiseSpeedKnots;
+        }
+
+        private static double FuelFlowTonPerHour(double grossWeightTon)
+        {
+            return (17.271 * grossWeightTon + 114.62) * 2.0 / 1000.0;
+        }
+    }
+}
