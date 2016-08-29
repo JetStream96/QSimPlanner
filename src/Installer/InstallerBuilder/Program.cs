9f9800a84e00cbaf8cd6856fa7de3c13a331a227
David Chen <davidchen9568@gmail.com>
2016-08-29 23:16:34 +0800

    Remove unneeded usings.

--- src/Installer/InstallerBuilder/Program.cs
+++ src/Installer/InstallerBuilder/Program.cs
@@ -1,14 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Diagnostics;
-using System.Reflection;
-using System.Threading;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
 
 namespace InstallerBuilder
 {
