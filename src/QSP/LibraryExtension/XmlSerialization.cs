9f8ec37514be449fa0ed77e64833315654d85b55
David Chen <davidchen9568@gmail.com>
2016-09-02 22:16:37 +0800

    Add methods for xml serialization.

---
+++ src/QSP/LibraryExtension/XmlSerialization.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace QSP.LibraryExtension
+{
+    public class XmlSerialization
+    {
+        public static T Deserialize<T>(string data)
+        {
+            var deserializer = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(data))
+            {
+                return (T)deserializer.Deserialize(reader);
+            }
+        }
+
+        public static string Serialize<T>(object o)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, o);
+                return writer.ToString();
+            }
+        }
+    }
+}
