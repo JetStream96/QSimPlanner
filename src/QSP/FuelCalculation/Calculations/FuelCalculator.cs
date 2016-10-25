8f115e3606c30c69e5ac59996ac94d6622050369
David Chen <davidchen9568@gmail.com>
2016-10-26 00:24:22 +0800

    Implement parameter computation methods for FuelDataItem. Some implementation of FuelCalc.

--- src/QSP/FuelCalculation/Calculations/FuelCalculator.cs
+++ src/QSP/FuelCalculation/Calculations/FuelCalculator.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using QSP.WindAloft;
 using QSP.RouteFinding.Routes;
 using QSP.FuelCalculation.Results;
+using QSP.RouteFinding.Airports;
+using QSP.FuelCalculation.FuelDataNew;
+using static System.Math;
 
 namespace QSP.FuelCalculation.Calculations
 {
+    // The first and last waypoints in route must be airports or runways.
+    // The ident must start with the ICAO code of airport.
     /// <summary>
     /// Computes the actual fuel burn for the route.
     /// </summary>
     public class FuelCalculator
     {
+        // private const deltaDis
+        private AirportManager airportList;
+        private CrzAltProvider altProvider;
         private IWindTableCollection windTable;
         private Route route;
         private FuelDataNew.FuelDataItem fuelData;
 
         public FuelCalculator(
+            AirportManager airportList,
+            CrzAltProvider altProvider,
             IWindTableCollection windTable,
             Route route,
             FuelDataNew.FuelDataItem fuelData)
         {
             if (route.Count < 2) throw new ArgumentException();
+
+            this.airportList = airportList;
+            this.altProvider = altProvider;
             this.windTable = windTable;
             this.route = route;
             this.fuelData = fuelData;
@@ -31,10 +44,41 @@ namespace QSP.FuelCalculation.Calculations
             double landingFuelKg,
             double maxAltFt)
         {
+            // Initialize variables.
             var waypoints = new List<PlanNode>();
 
             var node = route.Last;
-            //waypoints.Add(new DetailedPlan.Node(node, RouteFinding.Containers.AirwayType.))
+            var wpt = node.Value.Waypoint;
+            double grossWtKg = zfwKg + landingFuelKg;
+            double timeRemainMin = 0.0;
+            var destIcao = wpt.ID.Substring(0, 4).ToUpper();
+            double altFt = airportList[destIcao].Elevation;
+            double fuelOnBoardKg = landingFuelKg;
+            double optCrzAltFt, atcAllowedAltFt, targetAltFt, fuelFlowPerMinKg,
+                descentGrad;
+            bool isDescending;
+
+            // Do computations.
+            optCrzAltFt = fuelData.OptCruiseAltFt(grossWtKg);
+            atcAllowedAltFt = altProvider.ClosestAltitudeFt(
+                wpt, node.Next.Value.Waypoint, optCrzAltFt);
+            targetAltFt = Min(atcAllowedAltFt, maxAltFt);
+            isDescending = Abs(altFt - targetAltFt) > 0.1;
+
+            if (isDescending)
+            {
+                fuelFlowPerMinKg = fuelData.DescentFuelPerMinKg(grossWtKg);
+                descentGrad = fuelData.DescentGradient(grossWtKg);
+            }
+            else
+            {
+                fuelFlowPerMinKg = fuelData.CruiseFuelPerMinKg(grossWtKg);
+                descentGrad = 0.0;
+            }
+
+
+            waypoints.Add(new PlanNode(node.Value, ))
+
             throw new NotImplementedException();
         }
     }
--- src/QSP/FuelCalculation/FuelDataNew/FuelDataItemExtension.cs
+++ src/QSP/FuelCalculation/FuelDataNew/FuelDataItemExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace QSP.FuelCalculation.FuelDataNew
+﻿using QSP.MathTools.Interpolation;
+
+namespace QSP.FuelCalculation.FuelDataNew
 {
     public static class FuelDataItemExtension
     {
@@ -20,6 +22,105 @@
             double fuelkg, double weightTon)
         {
             return fuelkg / item.HoldingFuelPerMininuteKg(weightTon);
+        }
+
+        public static double CruiseFuelPerMinKg(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.CruiseFuelPerMinKg, p2.CruiseFuelPerMinKg);
+        }
+
+        public static double CruiseKias(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.CruiseKias, p2.CruiseKias);
+        }
+
+        public static double ClimbGradient(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.ClimbGradient, p2.ClimbGradient);
+        }
+
+        public static double ClimbFuelPerMinKg(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.ClimbFuelPerMinKg, p2.ClimbFuelPerMinKg);
+        }
+
+        public static double DescentGradient(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.DescentGradient, p2.DescentGradient);
+        }
+
+        public static double DescentFuelPerMinKg(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.DescentFuelPerMinKg, p2.DescentFuelPerMinKg);
+        }
+
+        public static double OptCruiseAltFt(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.OptCruiseAltFt, p2.OptCruiseAltFt);
+        }
+
+        public static double EtopsCruiseKtas(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.EtopsCruiseKtas, p2.EtopsCruiseKtas);
+        }
+
+        public static double EtopsCruiseFuelPerMinKg(
+            this FuelDataItem item, double grossWeightKg)
+        {
+            var p1 = item.DataPoint1;
+            var p2 = item.DataPoint2;
+
+            return Interpolate1D.Interpolate(
+                p1.WeightKg, p2.WeightKg, grossWeightKg,
+                p1.EtopsCruiseFuelPerMinKg, p2.EtopsCruiseFuelPerMinKg);
         }
     }
 }
