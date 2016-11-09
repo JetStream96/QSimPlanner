ebd0b5e5528fab729a1112d20952a010c57380b4
David Chen <davidchen9568@gmail.com>
2016-11-09 23:11:09 +0800

    Improve readability and fix some issues.

--- src/QSP/FuelCalculation/Calculations/BasicCrzAltProvider.cs
+++ src/QSP/FuelCalculation/Calculations/BasicCrzAltProvider.cs
@@ -4,49 +4,67 @@ using static System.Math;
 
 namespace QSP.FuelCalculation.Calculations
 {
-    // TODO: This implementation is temporary.
+    // TODO: Add new class for more sophisticated implementation.
+    // TODO: Westbound: Even altitudes
+    // Eastbound: Odd altitudes
+    // For metric system, i.e. China, it will be complicated.
+
     public class BasicCrzAltProvider : ICrzAltProvider
     {
+        // Westbound altitudes: 2000 4000 ... 40000 43000 47000
+        // Eastbound altitudes: 3000 5000 ... 41000 45000 49000
+
         public double ClosestAlt(
-            ICoordinate c, double heading, double altitude)
+            ICoordinate c, double heading, double alt)
         {
-            // TODO: Westbound: Even altitudes
-            // Eastbound: Odd altitudes
-            // For metric system, i.e. China, it will be complicated.
-
-            bool isWestBound = 0.0 <= heading && heading < 180.0;
-            var sep = Seperation(altitude);
-            var altBorder = isWestBound ? 41500.0 : 41000.0;
-            double adjustment;
-
-            if (altitude <= altBorder)
+            if (0.0 <= heading && heading < 180.0)
             {
-                adjustment = isWestBound ? 0.0 : 0.5;
+                if (alt <= 40000.0) return Round(alt / 2000.0) * 2000.0;
+                if (alt >= 43000.0) return Round(alt / 4000.0) * 4000.0;
+                return alt >= 41500.0 ? 43000.0 : 40000.0;
             }
             else
             {
-                adjustment = isWestBound ? 0.75 : 0.25;
-            }
+                if (alt <= 41000.0)
+                {
+                    return Round((alt - 1000.0) / 2000.0) * 2000.0 + 1000.0;
+                }
 
-            return (Round(altitude / sep - adjustment) + adjustment) * sep;
+                return Round((alt - 1000.0) / 4000.0) * 4000.0 + 1000.0;
+            }
         }
 
         public double ClosestAltBelow(
-            ICoordinate c, double heading, double altitude)
+            ICoordinate c, double heading, double alt)
         {
-            // TODO: Wrong.
-            return ClosestAlt(c, heading, altitude) - Seperation(altitude);
+            if (0.0 <= heading && heading < 180.0)
+            {
+                if (alt <= 40000.0) return FloorExclusive(alt / 2000.0) * 2000.0;
+                if (alt >= 43000.0) return FloorExclusive(alt / 4000.0) * 4000.0;                  
+                return 40000.0;
+            }
+            else
+            {
+                if (alt <= 41000.0)
+                {
+                    return FloorExclusive((alt - 1000.0) / 2000.0) * 2000.0 + 1000.0;
+                }
+
+                return FloorExclusive((alt - 1000.0) / 4000.0) * 4000.0 + 1000.0;
+            }
+        }
+
+        private static double FloorExclusive(double d)
+        {
+            var result = Floor(d);
+            if (result == d) result--;
+            return result;
         }
 
         public bool IsValidCrzAlt(
             ICoordinate c, double heading, double altitude)
         {
             return Abs(ClosestAlt(c, heading, altitude) - altitude) < 1.0;
-        }
-
-        public double Seperation(double alt)
-        {
-            return alt <= 41000.0 ? 2000.0 : 4000.0;
         }
     }
 }
--- src/Tests/UnitTest/FuelCalculation/Calculations/BasicCrzAltProviderTest.cs
+++ src/Tests/UnitTest/FuelCalculation/Calculations/BasicCrzAltProviderTest.cs
@@ -23,5 +23,20 @@ namespace UnitTest.FuelCalculation.Calculations
             Assert.AreEqual(40000.0, p.ClosestAlt(c, west, 41400.0));
             Assert.AreEqual(45000.0, p.ClosestAlt(c, east, 43600.0));
         }
+
+        [Test]
+        public void ClosestAltBelowTest()
+        {
+            var p = new BasicCrzAltProvider();
+            var c = new LatLon(0.0, 0.0);
+            var west = 100.0;
+            var east = 320.0;
+
+            Assert.AreEqual(23000.0, p.ClosestAltBelow(c, east, 25000.0));
+            Assert.AreEqual(24000.0, p.ClosestAltBelow(c, west, 25000.0));
+            Assert.AreEqual(39000.0, p.ClosestAltBelow(c, east, 41000.0));
+            Assert.AreEqual(40000.0, p.ClosestAltBelow(c, west, 43000.0));
+            Assert.AreEqual(41000.0, p.ClosestAltBelow(c, east, 45000.0));
+        }
     }
 }
