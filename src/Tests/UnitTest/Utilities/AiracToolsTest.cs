5a3f7fd93d97b27a8fb3cbe9389993181c471ce0
David Chen <davidchen9568@gmail.com>
2016-12-17 16:22:51 +0800

    Add tests for AiracPeriod.

---
+++ src/Tests/UnitTest/Utilities/AiracToolsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using static QSP.Utilities.AiracTools;
+
+namespace UnitTest.Utilities
+{
+    [TestFixture]
+    public class AiracToolsTest
+    {
+        [Test]
+        public void IncorrectAiracFormatShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => ParsePeriod("6JUN23JUL/14"));
+        }
+
+        [Test]
+        public void ParseAiracPeriodTest()
+        {
+            var p = ParsePeriod("26JUN23JUL/14");
+            var s = p.Start;
+            var e = p.End;
+            Assert.IsTrue(s.Year == 2014 && s.Month == 6 && s.Day == 26);
+            Assert.IsTrue(e.Year == 2014 && e.Month == 7 && e.Day == 23);
+        }
+
+        [Test]
+        public void ParseAiracPeriodCrossYear()
+        {
+            var p = ParsePeriod("08DEC05JAN/16");
+            var s = p.Start;
+            var e = p.End;
+            Assert.IsTrue(s.Year == 2016 && s.Month == 12 && s.Day == 8);
+            Assert.IsTrue(e.Year == 2017 && e.Month == 1 && e.Day == 5);
+        }
+    }
+}
