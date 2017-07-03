145381ed6d98a07985fc7d8cace33721a47d38ed
David Chen <davidchen9568@gmail.com>
2017-07-03 12:13:32 +0800

    Improve exception handling and logging.

--- src/TrackBackupApp/Global.asax.cs
+++ src/TrackBackupApp/Global.asax.cs
@@ -10,11 +10,12 @@ namespace TrackBackupApp
 {
     public class Global : HttpApplication
     {
-        private readonly double refreshIntervalMin = 1.0;
-        private readonly string DummyPageUrl = "/DummyPage.aspx";
-        private const string DummyCacheItemKey = "dummyKey";
+        private readonly double refreshIntervalSec = 60 * 10;
+        private readonly string dummyPageUrl = "/DummyPage.aspx";
+        private const string dummyCacheItemKey = "dummyKey";
 
         private static string serverUrl;
+        private static string unloggedError = "";
         private static DateTime lastUpdateTimeWest = new DateTime();
         private static DateTime lastUpdateTimeEast = new DateTime();
 
@@ -28,124 +29,176 @@ namespace TrackBackupApp
 
         private bool RegisterCacheEntry()
         {
-            if (HttpContext.Current.Cache[DummyCacheItemKey] != null)
+            if (HttpContext.Current.Cache[dummyCacheItemKey] != null)
             {
                 return false;
             }
 
             var onRemove = new CacheItemRemovedCallback(CacheItemRemovedCallback);
 
-            HttpContext.Current.Cache.Add(DummyCacheItemKey,
+            HttpContext.Current.Cache.Add(dummyCacheItemKey,
                                           "Test",
                                           null,
-                                          DateTime.Now.AddMinutes(refreshIntervalMin),
+                                          DateTime.Now.AddSeconds(refreshIntervalSec),
                                           Cache.NoSlidingExpiration,
                                           CacheItemPriority.Normal,
                                           onRemove);
 
-            WriteToLog(3);
+            WriteToLog("Cache Added.");
             return true;
         }
 
         public void CacheItemRemovedCallback(string key, object value,
             CacheItemRemovedReason reason)
         {
-            WriteToLog(" Cache item callback, Reason: " + reason.ToString());
+            WriteToLog("Cache item callback, Reason: " + reason.ToString());
             DoWork();
-            HitPage();
+            TryAndLogIfFail(HitPage);
+        }
+
+        // Does not throw exception.
+        private void TryAndLogIfFail(Action a)
+        {
+            try
+            {
+                a();
+            }
+            catch (Exception e)
+            {
+                WriteToLog(e.ToString());
+            }
         }
 
         private void HitPage()
         {
-            new WebClient().DownloadData(serverUrl + DummyPageUrl);
+            new WebClient().DownloadData(serverUrl + dummyPageUrl);
         }
 
+        // Does not throw exception.
         private void DoWork()
+        {
+            TryAndLogIfFail(SaveNats);
+        }
+
+        // Does not throw exception.
+        public void WriteToLog(string msg)
         {
             try
             {
-                SaveNats();
+                WriteLogFile(msg);
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                WriteToLog(ex.ToString());
+                unloggedError += AddTimeStamp(msg) + "\n" + AddTimeStamp(e.ToString());
             }
         }
 
-        private void WriteToLog(int para)
-        {
-            var msgs = new[]
-            {
-                " Application is starting.",
-                " Saving the NATs.",
-                " Cache item callback.",
-                " Cache Added."
-            };
-
-            WriteToLog(msgs[para]);
-        }
-
-
-        public void WriteToLog(string msg)
+        private void WriteLogFile(string msg)
         {
             using (var wr = File.AppendText(HostingEnvironment.MapPath("~/log.txt")))
             {
-                wr.WriteLine(DateTime.Now.ToString() + " " + msg);
+                wr.WriteLine(AddTimeStamp(msg));
             }
+        }
+
+        private string AddTimeStamp(string msg)
+        {
+            return DateTime.Now.ToString() + "  " + msg;
         }
 
         private void SaveNats()
         {
             var result = new NatsDownloader().DownloadFromNotam();
             Directory.CreateDirectory(HostingEnvironment.MapPath("~/nats"));
+            bool westUpdated = false;
+            bool eastUpdated = false;
 
             foreach (var i in result)
             {
                 if (i.Direction == NatsDirection.East)
                 {
-                    var filepath = "~/nats/Eastbound.xml";
-                    var (success, newTime) = NatsMessage.ParseDate(i.LastUpdated);
-                    if (success && newTime > lastUpdateTimeEast)
-                    {
-                        lastUpdateTimeEast = newTime;
-                        File.WriteAllText(HostingEnvironment.MapPath(filepath),
-                            i.ConvertToXml().ToString());
-                    }
+                    eastUpdated = SaveEastbound(i);
                 }
                 else
                 {
-                    var filepath = "~/nats/Westbound.xml";
-                    var (success, newTime) = NatsMessage.ParseDate(i.LastUpdated);
-                    if (success && newTime > lastUpdateTimeWest)
-                    {
-                        lastUpdateTimeWest = newTime;
-                        File.WriteAllText(HostingEnvironment.MapPath(filepath),
-                            i.ConvertToXml().ToString());
-                    }
+                    westUpdated = SaveWestbound(i);
                 }
             }
 
-            WriteToLog(1);
+            WriteToLog(SaveNatsMsg(westUpdated, eastUpdated));
+        }
+
+        // Test if the eastbound track needs to be saved. If yes, saved the file and 
+        // return true. Otherwise, returns false.
+        private bool SaveEastbound(IndividualNatsMessage i)
+        {
+            var filepath = "~/nats/Eastbound.xml";
+            var (success, newTime) = NatsMessage.ParseDate(i.LastUpdated);
+            if (success && newTime > lastUpdateTimeEast)
+            {
+                lastUpdateTimeEast = newTime;
+                File.WriteAllText(HostingEnvironment.MapPath(filepath),
+                    i.ConvertToXml().ToString());
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool SaveWestbound(IndividualNatsMessage i)
+        {
+            var filepath = "~/nats/Westbound.xml";
+            var (success, newTime) = NatsMessage.ParseDate(i.LastUpdated);
+            if (success && newTime > lastUpdateTimeWest)
+            {
+                lastUpdateTimeWest = newTime;
+                File.WriteAllText(HostingEnvironment.MapPath(filepath),
+                    i.ConvertToXml().ToString());
+                return true;
+            }
+
+            return false;
+        }
+
+        private string SaveNatsMsg(bool westUpdated, bool eastUpdated)
+        {
+            if (westUpdated)
+            {
+                return eastUpdated ? "Both directions updated." : "Westbound updated.";
+            }
+
+            return eastUpdated ? "Eastbound updated." : "Neither direction updated.";
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
             SetServerUrl();
 
-            if (HttpContext.Current.Request.Url.PathAndQuery.Equals(
-                DummyPageUrl, StringComparison.OrdinalIgnoreCase))
+            var pq = HttpContext.Current.Request.Url.PathAndQuery.ToLower();
+
+            if (pq == dummyPageUrl.ToLower())
             {
-                // Add the item in cache And when succesful, do the work.
+                // Add the item in cache and when succesful, do the work.
                 RegisterCacheEntry();
             }
+            else if (pq == "/unloggederr")
+            {
+                RespondWithUnloggedErrors();
+            }
+        }
+
+        private void RespondWithUnloggedErrors()
+        {
+            Response.Write(unloggedError == "" ? "No unlogged error." : unloggedError);
+            Response.End();
         }
 
         protected void Application_Start(object sender, EventArgs e)
         {
             // Fires when the application is started
+            WriteToLog("Application started.");
             SaveNats();
             RegisterCacheEntry();
-            WriteToLog(0);
         }
 
         protected void Session_Start(object sender, EventArgs e)
