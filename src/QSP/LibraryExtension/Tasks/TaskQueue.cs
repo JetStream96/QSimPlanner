8729092df7c44e0ac8c3eef20826c00f1e52cb25
David Chen <davidchen9568@gmail.com>
2017-08-25 11:30:25 +0800

    Add comments for task queue.

--- src/QSP/LibraryExtension/Tasks/TaskQueue.cs
+++ src/QSP/LibraryExtension/Tasks/TaskQueue.cs
@@ -4,6 +4,18 @@ using System.Threading.Tasks;
 
 namespace QSP.LibraryExtension.Tasks
 {
+    // Two things to be proved:
+    // 1. No two tasks are executed at the same time:
+    // If a task is executing, IsRunning is necessarily ture. Therefore when a new task 
+    // is added, it won't be executed.
+    // 
+    // 2. If the queue will be eventually empty:
+    // When a new task is added, since the code other than 'await' runs on a single thread,
+    // the possibilities are:
+    // (1) Run() already returned. In this case the new task will clearly be executed.
+    // (2) The code inside Run() is still executing, i.e. awaiting a task.
+    //     The new task will be run because of the while loop.
+    //
     /// <summary>
     /// The tasks added to this queue are executed in the order of addition.
     /// </summary>
@@ -12,7 +24,8 @@ namespace QSP.LibraryExtension.Tasks
         private Queue<Func<Task>> tasks = new Queue<Func<Task>>();
 
         /// <summary>
-        /// Returns ture if any task is running or pending. Otherwise false.
+        /// Returns ture if any task is running or pending.
+        /// If accessed on another thread, this can be true even if no task is running or pending.
         /// </summary>
         public bool IsRunning { get; private set; } = false;
 
