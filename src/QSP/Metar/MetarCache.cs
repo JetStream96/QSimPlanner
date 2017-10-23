79d1d3417e8b780015d5d18a4f6be58f5016d5de
David Chen <davidchen9568@gmail.com>
2017-10-23 16:08:26 +0800

    Add metar cache.

---
+++ src/QSP/Metar/MetarCache.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSP.Metar
+{
+    public class MetarCache
+    {
+        public TimeSpan ExpireTime { get; private set; }
+        private Dictionary<string, MetarCacheItem> items = new Dictionary<string, MetarCacheItem>();
+
+        public MetarCache() : this(new TimeSpan(0, 10, 0)) { }
+
+        public MetarCache(TimeSpan expireTime)
+        {
+            this.ExpireTime = expireTime;
+        }
+
+        // Returns null if the item does not exist.
+        public MetarCacheItem GetItem(string icao)
+        {
+            if (items.TryGetValue(icao, out var val))
+            {
+                if (IsStillValid(val))
+                {
+                    return val;
+                }
+
+                items.Remove(icao);
+            }
+
+            return null;
+        }
+
+        private bool IsStillValid(MetarCacheItem item)
+        {
+            return item.CreationTime + ExpireTime <= DateTime.UtcNow;
+        }
+
+        public void AddItem(string icao, MetarCacheItem item)
+        {
+            items.Add(icao, item);
+        }
+    }
+}
---
+++ src/QSP/Metar/MetarCacheItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QSP.WindAloft;
+
+namespace QSP.Metar
+{
+    public class MetarCacheItem
+    {
+        public string Metar { get; private set; }
+        public Wind? Wind { get; private set; }
+        public int? Temp { get; private set; }
+        public bool PrecipitationExists { get; private set; }
+        public DateTime CreationTime { get; private set; }
+
+        public MetarCacheItem(string metar, Wind? wind, int? temp, 
+            bool precipitation, DateTime creationTime)
+        {
+            this.Metar = metar;
+            this.Wind = wind;
+            this.Temp = temp;
+            this.PrecipitationExists = precipitation;
+            this.CreationTime = creationTime;
+        }
+
+        public static MetarCacheItem Create(string metar)
+        {
+            return new MetarCacheItem(
+                metar,
+                ParaExtractor.GetWind(metar),
+                ParaExtractor.GetTemp(metar),
+                ParaExtractor.PrecipitationExists(metar),
+                DateTime.UtcNow);
+        }
+    }
+}
