c2cf43758c6d737283317d319660b3da38f3702e
David Chen <davidchen9568@gmail.com>
2017-11-04 02:01:19 +0800

    Add missed file.

--- src/QSP/RouteFinding/Airports/IRwyDataExtension.cs
+++ src/QSP/RouteFinding/Airports/IRwyDataExtension.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QSP.AviationTools;
 
 namespace QSP.RouteFinding.Airports
 {
-    class IRwyDataExtension
+    public static class IRwyDataExtension
     {
+        public static double LengthMeter(this IRwyData r) =>
+            r.ElevationFt * Constants.FtMeterRatio;
     }
 }
