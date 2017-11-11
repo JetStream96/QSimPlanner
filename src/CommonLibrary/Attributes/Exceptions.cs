5539a377b1845d70da79ee389d5222475956c4b5
David Chen <davidchen9568@gmail.com>
2017-11-11 11:54:13 +0800

    Add attributes for exceptions.

---
+++ src/CommonLibrary/Attributes/Exceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommonLibrary.Attributes
+{
+    /// <summary>
+    /// Indicates that the method may throw exception but the exception type is not important.
+    /// </summary>
+    public class Throws : Attribute { }
+
+    /// <summary>
+    /// Indicates that the method cannot throw exception.
+    /// </summary>
+    public class NoThrow : Attribute { }
+}
