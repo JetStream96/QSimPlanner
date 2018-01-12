d70e6af9302ce7ecd25b0ac335948d1d89daa227
David Chen <davidchen9568@gmail.com>
2018-01-12 21:03:50 +0800

    Add methods to easily initialize Sets and Lists.

--- src/CommonLibrary/LibraryExtension/Types.cs
+++ src/CommonLibrary/LibraryExtension/Types.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 
 namespace CommonLibrary.LibraryExtension
 {
+    /// <summary>
+    /// A more concise way to instantiate frequently used data structures.
+    /// </summary>
     public static class Types
     {
+        /// <summary>
+        /// Usage: ReadOnlySet("A", "BC")
+        /// </summary>
         public static IReadOnlySet<T> ReadOnlySet<T>(T x, params T[] xs)
         {
             if (xs.Length == 0) return new ReadOnlySet<T>(new HashSet<T> { x });
@@ -12,6 +18,16 @@ namespace CommonLibrary.LibraryExtension
             var set = new HashSet<T>(xs);
             set.Add(x);
             return new ReadOnlySet<T>(set);
+        }
+
+        /// <summary>
+        /// Usage: List(1, 2, 3) => [1, 2, 3]
+        /// </summary>
+        public static List<T> List<T>(T x, params T[] xs)
+        {
+            var list = new List<T> { x };
+            if (xs.Length > 0) list.AddRange(xs);
+            return list;
         }
     }
 }
---
+++ src/CommonLibraryTest/LibraryExtension/TypesTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using static CommonLibrary.LibraryExtension.Types;
+
+namespace CommonLibraryTest.LibraryExtension
+{
+    [TestFixture]
+    public class TypesTest
+    {
+        [Test]
+        public void ListTest()
+        {
+            var list = List(8, 12, 6.0);
+            var eps = 1e-7;
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(8, list[0], eps);
+            Assert.AreEqual(12, list[1], eps);
+            Assert.AreEqual(6.0, list[2], eps);
+        }
+
+        [Test]
+        public void ListSingleItemTest()
+        {
+            string[] array = { "A", "BC" };
+            var list = List(array);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("A", list[0][0]);
+            Assert.AreEqual("BC", list[0][1]);
+        }
+
+        [Test]
+        public void ReadOnlySetTest()
+        {
+            var set = ReadOnlySet(-5, 1);
+            Assert.AreEqual(2, set.Count);
+            Assert.IsTrue(set.Contains(-5));
+            Assert.IsTrue(set.Contains(1));
+        }
+
+        [Test]
+        public void ReadOnlySetSingleItemTest()
+        {
+            string[] array = { "A" };
+            var set = ReadOnlySet(array);
+            Assert.AreEqual(1, set.Count);
+            Assert.AreEqual(1, set.First().Length);
+            Assert.AreEqual("A", set.First()[0]);
+        }
+    }
+}
