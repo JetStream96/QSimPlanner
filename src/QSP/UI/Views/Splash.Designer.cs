037225510aad3a0ffbc1f6b1672459169c9c7181
David Chen <davidchen9568@gmail.com>
2018-01-21 13:13:59 +0800

    Update splash screen icon.

--- src/QSP/UI/Views/Splash.Designer.cs
+++ src/QSP/UI/Views/Splash.Designer.cs
@@ -36,6 +36,7 @@ namespace QSP.UI.Views
 		[System.Diagnostics.DebuggerStepThrough()]
 		private void InitializeComponent()
 		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
             this.Label1 = new System.Windows.Forms.Label();
             this.SmallTitleLbl = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -48,10 +49,9 @@ namespace QSP.UI.Views
             this.Label1.AutoSize = true;
             this.Label1.Font = new System.Drawing.Font("Segoe UI Semibold", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label1.ForeColor = System.Drawing.Color.White;
-            this.Label1.Location = new System.Drawing.Point(87, 10);
-            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Location = new System.Drawing.Point(72, 8);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(312, 65);
+            this.Label1.Size = new System.Drawing.Size(246, 51);
             this.Label1.TabIndex = 0;
             this.Label1.Text = "QSimPlanner";
             // 
@@ -61,10 +61,10 @@ namespace QSP.UI.Views
             this.SmallTitleLbl.AutoSize = true;
             this.SmallTitleLbl.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SmallTitleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.SmallTitleLbl.Location = new System.Drawing.Point(243, 85);
-            this.SmallTitleLbl.Margin = new System.Windows.Forms.Padding(4, 0, 100, 0);
+            this.SmallTitleLbl.Location = new System.Drawing.Point(196, 68);
+            this.SmallTitleLbl.Margin = new System.Windows.Forms.Padding(3, 0, 80, 0);
             this.SmallTitleLbl.Name = "SmallTitleLbl";
-            this.SmallTitleLbl.Size = new System.Drawing.Size(144, 32);
+            this.SmallTitleLbl.Size = new System.Drawing.Size(114, 25);
             this.SmallTitleLbl.TabIndex = 1;
             this.SmallTitleLbl.Text = "version 0.5.0";
             // 
@@ -76,25 +76,26 @@ namespace QSP.UI.Views
             this.tableLayoutPanel1.Controls.Add(this.SmallTitleLbl, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(487, 143);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(390, 114);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // Splash
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(487, 143);
+            this.ClientSize = new System.Drawing.Size(390, 114);
             this.ControlBox = false;
             this.Controls.Add(this.tableLayoutPanel1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Splash";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
