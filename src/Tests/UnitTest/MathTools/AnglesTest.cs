f46a90d13869dcd392aa609ca820ab8e78ed32c4
David Chen <davidchen9568@gmail.com>
2018-03-10 17:10:45 +0800

    Add missing tests.

---
+++ src/Tests/UnitTest/MathTools/AnglesTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using static QSP.MathTools.Angles;
+
+namespace UnitTest.MathTools
+{
+    [TestFixture]
+    public class AnglesTest
+    {
+        [Test]
+        public void ToDegreeTest()
+        {
+            Assert.AreEqual(180.0, ToDegree(Math.PI), 1e-8);
+        }
+
+        [Test]
+        public void ToRadianTest()
+        {
+            Assert.AreEqual(2 * Math.PI, ToRadian(360.0), 1e-8);
+        }
+    }
+}
