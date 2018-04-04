d75d438c058902f7a1e2a4a379be828174ce7c02
David Chen <davidchen9568@gmail.com>
2018-04-05 00:48:29 +0800

    temp commit

---
+++ src/A319Fuel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace A319Fuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        // Convert to Boeing format.
+        static void Convert()
+        {
+            var doc = XDocument.Load("./profile/A319_100_CFM.xml");
+
+        }
+    }
+}
