dc45ee55061efa899ea5d5d5fb86714186f69a4a
David Chen <davidchen9568@gmail.com>
2018-04-14 11:47:04 +0800

    Add flightfactor A320 format.

---
+++ src/QSP/RouteFinding/FileExport/Providers/FlightFactorA320Provider.cs
@@ -0,0 +1,32 @@
+﻿using QSP.LibraryExtension;
+using QSP.RouteFinding.Routes;
+using System;
+using System.Linq;
+using static QSP.LibraryExtension.Types;
+
+namespace QSP.RouteFinding.FileExport.Providers
+{
+    public static class FlightFactorA320Provider
+    {
+        /// <summary>
+        /// Get string of the flight plan to export.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public static string GetExportText(Route route)
+        {
+            if (route.Count < 2) throw new ArgumentException();
+            var from = route.FirstWaypoint.ID.Substring(0, 4);
+            var to = route.LastWaypoint.ID.Substring(0, 4);
+
+            var str = route.ToString(true);
+            if (str == "DCT") return $"RTE {from}{to}01 {from} {to}";
+            
+            // Replace SID/STAR with DCT.
+            var split = str.Split(' ');
+            if (split.Length < 3) throw new ArgumentException();
+            var middle= split.WithoutFirstAndLast();
+            var text = string.Join(" ", List("DCT").Concat(middle).Concat(List("DCT")));
+            return $"RTE {from}{to}01 {from} {text} {to}";
+        }
+    }
+}
---
+++ src/Tests/UnitTest/RouteFinding/FileExport/Providers/FlightFactorA320ProviderTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using QSP.LibraryExtension;
+using QSP.RouteFinding.Containers;
+using QSP.RouteFinding.FileExport.Providers;
+
+namespace UnitTest.RouteFinding.FileExport.Providers
+{
+    [TestFixture]
+    public class FlightFactorA320ProviderTest
+    {
+        [Test]
+        public void GetExportTextTest()
+        {
+            var route = Common.GetRoute(
+                new Waypoint("RJBB06L", 0.0, 0.0), "A", -1.0,
+                new Waypoint("WPT0", 0.0, 10.0), "B", -1.0,
+                new Waypoint("WPT1", 0.0, -20.0), "DCT", -1.0,
+                new Waypoint("WPT2", 0.0, 2.5), "C", -1.0,
+                new Waypoint("RJAA18", 0.0, 3.0));
+
+            var text = FlightFactorA320Provider.GetExportText(route);
+
+            var expected = "RTE RJBBRJAA01 RJBB DCT WPT0 B WPT1 DCT WPT2 DCT RJAA";
+
+            Assert.IsTrue(expected.EqualsIgnoreNewlineStyle(text));
+        }
+
+        [Test]
+        public void GetExportTextNoWaypontDoesNotThrow()
+        {
+            var route = Common.GetRoute(
+                new Waypoint("RJBB06L", 0.0, 0.0), "DCT", -1.0,
+                new Waypoint("RJAA18", 0.0, 3.0));
+
+            var text = FlightFactorA320Provider.GetExportText(route);
+
+            var expected = "RTE RJBBRJAA01 RJBB RJAA";
+
+            Assert.IsTrue(expected.EqualsIgnoreNewlineStyle(text));
+        }
+    }
+}
